c031141eaf6d16fab1fe7e5e7a1fac0b0037eea0
Sean Wallitsch <shidarin@alphamatte.com>
2014-07-12 12:11:16 -0700

    Further fixes short description

--- dehead/__init__.py
+++ dehead/__init__.py
@@ -109,7 +109,7 @@ __credits__ = ["Sean Wallitsch", ]
 __license__ = "MIT"
 __version__ = "0.1.1"
 __module_name__ = "dehead"
-__short_desc__ = "A script to extract div's with class of 'section' from HTML"
+__short_desc__ = "A script to extract the main div section element from HTML"
 __status__ = "Development"
 __url__ = "http://github.com/shidarin/dehead"
 
